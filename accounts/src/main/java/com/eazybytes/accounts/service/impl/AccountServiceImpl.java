1863b390d7b442a1227c4935d544aee8fbf7fc20
Rahul Sahani <srahulsahani@gmail.com>
2024-06-11 01:14:34 +0530

    impl logic for customer account creation

--- accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -1,11 +1,17 @@
 package com.eazybytes.accounts.service.impl;
 
+import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +26,28 @@ public class AccountServiceImpl implements IAccountsService {
      */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
 
+        accountsRepository.save(createNewAccount(savedCustomer));
+
+    }
+
+
+    /**
+     * @param customer -Customer Object
+     * @return the new account details
+     * randomAccountNumber - generate 10 digit random account number
+     */
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccount = new Accounts();
+        newAccount.setAccountNumber(customer.getCustomerId());
+
+        
+        long randomAccountNumber = 1000000000L + new Random().nextInt(900000000);
+        newAccount.setAccountNumber(randomAccountNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
     }
 }

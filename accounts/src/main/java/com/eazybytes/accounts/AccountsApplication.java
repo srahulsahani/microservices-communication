adc2d3d677738c71725a6e650596f4d637757d9a
Rahul Sahani <srahulsahani@gmail.com>
2024-06-16 17:06:28 +0530

    @OpenAPIDefination, SWAGGER UI Documentation

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,33 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account microservices REST API documentation",
+				description = "EazyBank Accounts microservices REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Rahul Sahani",
+						email = "rahul@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservices REST API Documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
